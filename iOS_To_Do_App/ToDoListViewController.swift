6cf7f207392903990a132d47f5ec4dcd8b846177
jitzzz1996 <43922185+jitzzz1996@users.noreply.github.com>
2021-12-23 20:35:29 +0530

    table view setup done

---
+++ iOS_To_Do_App/ToDoListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS_To_Do_App
+//
+//  Created by jitesh bablani on 23/12/21.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let listData = ["abc","def","ghi"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        listData.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
+        cell.textLabel?.text = listData[indexPath.row]
+        return cell
+    }
+
+}
+
